c8543dc6b6a68e25ea172873a825dd7e3d096493
Lilobotx <Lilobotx@gmail.com>
2026-05-11 14:29:39 +0200

    W button. Gemerald alert.

---
+++ script.js
@@ -0,0 +1,8 @@
+const PNGButton = document.getElementById("PNGButton");
+const JPGButton = document.getElementById("JPGButton");
+const GIFButton = document.getElementById("GIFButton");
+const MP4Button = document.getElementById("MP4Button");
+const WEBPButton = document.getElementById("WEBPButton");
+const SVGButton = document.getElementById("SVGButton");
+
+PNGButton.addEventListener("click", function (event) {});
